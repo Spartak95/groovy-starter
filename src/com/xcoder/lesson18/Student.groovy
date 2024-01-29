366876db9d4b2d1a97891e042143560da618a916
spartak.gareginyan <gareginyans@mail.ru>
2024-01-29 14:53:17 +0400

    meta object protocol.

--- src/com/xcoder/lesson18/Student.groovy
+++ src/com/xcoder/lesson18/Student.groovy
@@ -1,10 +1,16 @@
 package com.xcoder.lesson18
 
+import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 import groovy.transform.TupleConstructor
 
 @ToString
 @TupleConstructor
+@EqualsAndHashCode
+//@Immutable
+//@Builder
+//@Slf4j
+// @Canonical -> EqualsAndHashCode, TupleConstructor, ToString
 class Student implements WithId {
     String firstName
     String lastName
---
+++ src/com/xcoder/lesson21/MopRunner.groovy
@@ -0,0 +1,8 @@
+package com.xcoder.lesson21
+
+// Meta Object Protocol(MOP)
+class MopRunner {
+    static void main(String[] args) {
+        println("Hello")
+    }
+}
